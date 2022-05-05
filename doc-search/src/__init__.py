b945f22a6434e49d9558a21fbb1cb31a37eacf94
netanelkoli <netanelkoli@gmail.com>
2022-05-06 02:54:43 +0300

    Finish tasks 1,2

--- doc-search/src/__init__.py
+++ doc-search/src/__init__.py
@@ -1,10 +1,16 @@
-import sys
+import sys, os, time
 from index import Index
 from bottle import route, run, template, request
 
+SLEEP_TIME=60
 
 if __name__ == '__main__':
     index = Index.new(sys.argv[1])
+    test_load = os.environ.get('TEST_LOAD', default='false')
+    print(f"Simulating a heavy load on start-up: {test_load}")
+
+    if test_load == 'true':
+        time.sleep(SLEEP_TIME)
 
     @route('/')
     def search():
@@ -12,4 +18,9 @@ if __name__ == '__main__':
         print(q, type(q))
         return dict(results=list(index.search(str(q))))
 
-    run()
+
+    @route('/health')
+    def health():
+        return "healthy"
+
+    run(host="0.0.0.0")
