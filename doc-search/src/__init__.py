bbe540462d98109c33d3827b40db2a9700ef5108
netanelkoli <netanel.koli@argus-sec.com>
2022-05-06 12:50:25 +0300

    Convert the app to flask instead of bottle, to use the default exporter

--- doc-search/src/__init__.py
+++ doc-search/src/__init__.py
@@ -1,26 +1,28 @@
 import sys, os, time
 from index import Index
-from bottle import route, run, template, request
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 SLEEP_TIME=60
+app = Flask(__name__)
+index = Index.new(sys.argv[1])
+metrics = PrometheusMetrics(app)
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+@app.route('/')
+def search():
+    q = request.args.get('q', default="")
+    print(q, type(q))
+    return dict(results=list(index.search(str(q))))
+
+@app.route('/health')
+def health():
+    return "healthy"
 
 if __name__ == '__main__':
-    index = Index.new(sys.argv[1])
     test_load = os.environ.get('TEST_LOAD', default='false')
     print(f"Simulating a heavy load on start-up: {test_load}")
-
     if test_load == 'true':
         time.sleep(SLEEP_TIME)
-
-    @route('/')
-    def search():
-        q = request.query.q
-        print(q, type(q))
-        return dict(results=list(index.search(str(q))))
-
-
-    @route('/health')
-    def health():
-        return "healthy"
-
-    run(host="0.0.0.0")
+    app.run(port=8080, host="0.0.0.0")
